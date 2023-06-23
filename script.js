dfd885056bd65653d103a41a9fc4cf62c1a47b22
charlesherve03 <charlesherve10@gmail.com>
2023-06-23 11:18:46 +0100

    Create script.js
    Ajout de dependance javascript

---
+++ script.js
@@ -0,0 +1,2 @@
+{/* <script src="https://unpkg.com/@esri/arcgis-rest-request@4.0.0/dist/bundled/request.umd.js"></script>
+<script src="https://unpkg.com/@esri/arcgis-rest-routing@4.0.0/dist/bundled/routing.umd.js"></script> */}
